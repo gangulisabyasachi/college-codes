2b006aa549b839206c25f2461a63953aeb8bf607
gangulisabyasachi <ganguli.sabyasachi2705@gmail.com>
2024-09-14 15:27:53 +0530

    No ds store

--- 3rd sem/R/lab assignment/day4/day4p6.R
+++
@@ -1,2 +0,0 @@
-auto <- read.csv("/Users/sabyasachiganguli/Desktop/code/college codes/3rd sem/R/lab assignment/pdf/auto-mpg.csv", header = TRUE)
-barplot(auto$mpg, main = "MPG Plot", xlab = "mpg", col = "black", horiz = FALSE)
--- dost log ka codes/main1.py
+++
@@ -1,9 +0,0 @@
-from turtle import *
-speed(1000)
-color('red')
-bgcolor('green')
-b=200
-while b>0:
-    left(b)
-    forward(b*3)
-    b=b-1
